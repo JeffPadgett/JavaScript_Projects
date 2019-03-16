9e1bc76ea7488565455588dbf2b6f57b0b3fcef9
JeffPadgett <JeffreyPadgett1@gmail.com>
2019-03-16 16:56:42 -0400

    css active player class stylization

--- Small Projects/Pig_Game/starter/app.js
+++ Small Projects/Pig_Game/starter/app.js
@@ -13,7 +13,7 @@ var scores, roundScore, activePlayer;
 
 scores = [0,0];
 roundScore = 0;
-activePlayer = 1;
+activePlayer = 0;
 
 document.querySelector('.dice').style.display = 'none';
 document.getElementById('score-0').textContent = '0';
@@ -32,6 +32,20 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
     diceDOM.src = 'dice-' + dice + '.png';
 
     //3. Update the round score IF the rolled number is NOT a 1
+    if (dice !== 1){
+        //Add score
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+        //Next Player
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+        document.querySelector('.player-0-panel').classList.remove('active');
+        document.querySelector('.player-1-panel').classList.add('active');
+        
+    }
 
 
     
